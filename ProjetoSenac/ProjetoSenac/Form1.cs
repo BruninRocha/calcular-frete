f3295c1479a56f90d65995d2895750cbf060080d
BruninRocha <104468908+BruninRocha@users.noreply.github.com>
2022-07-29 19:10:22 -0300

    Add files via upload

---
+++ ProjetoSenac/ProjetoSenac/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoSenac
+{
+    public partial class Form1 : Form
+    {
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            Limpar();
+        }
+
+        void Calcular()
+        {
+
+        }
+
+        void Limpar()
+        {
+            // apagar o texto dos 3 campos
+            txtNome.Text = "";   
+        }
+    }
+}
